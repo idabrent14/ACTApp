b2f294d6ffcf4465bae0a3a3dd2557d368522bdf
Brent Ida <idabrent14@gmail.com>
2017-12-18 15:40:17 -0800

    Backend CRUD for the English section complete

---
+++ ACTApp/ACTApp.Models/view/EnglishViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTApp.Models.view
+{
+    public class EnglishViewModel
+    {
+        public int UserId { get; set; }
+        public int EnglishId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Passage1 { get; set; }
+        public string Passage2 { get; set; }
+        public string Passage3 { get; set; }
+        public string Passage4 { get; set; }
+        public string Passage5 { get; set; }
+    }
+}
---
+++ ACTApp/ACTApp.Web/Controllers/Api/EnglishController.cs
@@ -0,0 +1,66 @@
+﻿using ACTApp.Models.domain;
+using ACTApp.Models.responses;
+using ACTApp.Models.view;
+using ACTApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ACTApp.Web.Controllers.Api
+{
+    [RoutePrefix("api/english")]
+    public class EnglishController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Insert(EnglishModel model)
+        {
+            try
+            {
+                EnglishService svc = new EnglishService();
+                ItemResponse<int> resp = new ItemResponse<int>();
+                resp.Item = svc.EnglishInsert(model);
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("{userId}"), HttpGet]
+        public HttpResponseMessage SelectById(int userId)
+        {
+            try
+            {
+                EnglishService svc = new EnglishService();
+                ItemsResponse<EnglishViewModel> resp = new ItemsResponse<EnglishViewModel>();
+                resp.Items = svc.SelectById(userId); 
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Route("{englishId}"), HttpDelete]
+        public HttpResponseMessage Delete(int englishId)
+        {
+            try
+            {
+                EnglishService svc = new EnglishService();
+                svc.Delete(englishId);
+                SuccessResponse resp = new SuccessResponse();
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+    }
+}
