618eb8a432d9a335ca438e59481cbd177653517d
Brent Ida <idabrent14@gmail.com>
2017-12-24 13:43:06 -0800

    login implemented into the front end

--- ACTApp/ACTApp.Web/Scripts/login/login.controller.js
+++ ACTApp/ACTApp.Web/Scripts/login/login.controller.js
@@ -4,28 +4,30 @@
         .module("publicApp")
         .controller("loginController", LoginController);
 
-    LoginController.$inject = ["$scope", "loginService", "toaster"];
+    LoginController.$inject = ["$scope", "loginService", "toaster", "$location"];
 
-    function LoginController($scope, LoginService, toaster) {
+    function LoginController($scope, LoginService, toaster, $location) {
         var vm = this;
         vm.$scope = $scope;
         vm.loginService = LoginService;
-        vm.register = _register;
-        vm.registerSuccess = _registerSuccess;
-        vm.registerError = _registerError;
-        vm.registerUser = {};
+        vm.login = _login;
+        vm.loginSuccess = _loginSuccess;
+        vm.loginError = _loginError;
+        vm.loiginUser = {};
 
-        function _register() {
-            console.log("Register button clicked")
-            vm.registerService.register(vm.registerUser)
-                .then(vm.registerSuccess).catch(vm.registerError);
+        function _login() {
+            console.log("login button clicked")
+            vm.loginService.login(vm.loginUser)
+                .then(vm.loginSuccess).catch(vm.loginError);
         }
-        function _registerSuccess(res) {
+        function _loginSuccess(res) {
             console.log(res);
-            toaster.pop('success', "success", "Successfully Registered");
+            toaster.pop('success', "Success", "Successfully Logged in");
+            $location.path('/home');
         }
-        function _registerError(res) {
+        function _loginError(res) {
             console.log(res);
+            toaster.pop('error', 'Error', "Incorrect Email or Password")
         }
     }
 })();
