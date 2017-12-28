6e7696c9c815e46e7af8aca3f058dd6fcb8f41c1
Brent Ida <idabrent14@gmail.com>
2017-12-28 15:52:32 -0800

    Started working on the math section

---
+++ ACTApp/ACTApp.Services/MathService.cs
@@ -0,0 +1,57 @@
+﻿using ACTApp.Models.domain;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTApp.Services
+{
+    public class MathService
+    {
+        public int MathInsert(MathModel model)
+        {
+            int mathId = 0;
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("dbo.UsersMath_Insert", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UserId", model.UserId);
+                    cmd.Parameters.AddWithValue("@Easy", model.Easy);
+                    cmd.Parameters.AddWithValue("@Medium", model.Medium);
+                    cmd.Parameters.AddWithValue("@Hard", model.Hard);
+
+                    SqlParameter parm = new SqlParameter("@Id", SqlDbType.Int);
+                    parm.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(parm);
+                    cmd.ExecuteNonQuery();
+                    mathId = (int)cmd.Parameters["@Id"].Value;
+                };
+                conn.Close();
+            }
+            return mathId;
+        }
+
+        public void Delete(int mathId)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("dbo.UsersMath_Delete", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@MathId", mathId);
+                };
+                conn.Close();
+            }
+        }
+
+    }
+}
---
+++ ACTApp/ACTApp.Web/Controllers/Api/MathController.cs
@@ -0,0 +1,49 @@
+﻿using ACTApp.Models.domain;
+using ACTApp.Models.responses;
+using ACTApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ACTApp.Web.Controllers.Api
+{
+    [RoutePrefix("api/math")]
+    public class MathController : ApiController
+    {
+        [Route, HttpPost]
+        public HttpResponseMessage MathInsert(MathModel model)
+        {
+            try
+            {
+                MathService svc = new MathService();
+                ItemResponse<int> resp = new ItemResponse<int>();
+                resp.Item = svc.MathInsert(model);
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);    
+            }
+        }
+
+        [Route, HttpDelete]
+        public HttpResponseMessage MathDelete(int mathId)
+        {
+            try
+            {
+                MathService svc = new MathService();
+                svc.Delete(mathId);
+                SuccessResponse resp = new SuccessResponse();
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+    }
+}
